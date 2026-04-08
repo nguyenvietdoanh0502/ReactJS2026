c1e6c3eed42b2b7274c5fe7de791918a98310fe0
nguyenvietdoanh0502 <vietdoanh0502@gmail.com>
2026-04-08 18:37:52 +0700

    check

--- buoi1/src/App.jsx
+++ buoi1/src/App.jsx
@@ -1,7 +1,10 @@
 ﻿import BookList from './components/BookList'
+import React from 'react'
+import { useState, useMemo } from 'react'
 import ProductList from './components/ProductList'
 
 function App() {
+  const [fibonacci,setFibonacci] = useState(1)
   return (
     <main className="min-h-screen bg-slate-100 p-6">
       <BookList />
